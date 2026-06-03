3e95d0b15485f8b7d27eb9ca8c959c88b8fc3a2a
jainkavita31 <ks@taketwotechnologies.com>
2026-06-03 16:49:23 +0530

    Update index_snapshot.php

--- local/dashboard/classes/local/index_snapshot.php
+++ local/dashboard/classes/local/index_snapshot.php
@@ -18,6 +18,8 @@ namespace local_dashboard\local;
 
 defined('MOODLE_INTERNAL') || die();
 
+require_once(__DIR__ . '/../../lib.php');
+
 /**
  * Build and persist dashboard aggregates for fast index loads (quizid = all only).
  */
